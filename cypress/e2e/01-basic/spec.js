bde53033e792d0e8b125d73cb7a00122506485ca
MaxHam <45538872+MaxHam@users.noreply.github.com>
2022-06-17 17:53:02 +0200

    Translation of 01-basic

--- cypress/e2e/01-basic/spec.js
+++ cypress/e2e/01-basic/spec.js
@@ -1,23 +1,18 @@
 /// <reference types="cypress" />
 // @ts-check
+
+/**
+ * Dieser Test ruft die Testapplikation auf und soll überprüfen ob das Todo Input Feld, der Footer und die Überschrift der Website verfügbar sind?
+ * Dieser Test schlägt fehl. Kannst du den Fehler beheben und finden ?
+ *
+ * Tipp: Welchen Wert hat das h1 Element ?
+ */
+
 it('loads', () => {
-  // application should be running at port 3000
+  // Applikation läuft unter Port 3000
   cy.visit('localhost:3000')
 
-  // passing assertions
-  // https://on.cypress.io/get
   cy.get('.new-todo').get('footer')
 
-  // this assertion fails on purpose
-  // can you fix it?
-  // https://on.cypress.io/get
-  cy.contains('h1', 'todos')
-
-  // can you write "cy.contains" using regular expression?
-  // cy.contains('h1', /.../)
-
-  // also good practice is to use data attributes specifically for testing
-  // see https://on.cypress.io/best-practices#Selecting-Elements
-  // which play well with "Selector Playground" tool
-  // how would you do select this element?
+  cy.contains('h1', 'Todos App')
 })
